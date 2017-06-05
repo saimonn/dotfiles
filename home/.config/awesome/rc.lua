bae5847e01fffe101a4f4f211ef401b3e0438aa6
Simon Séhier <simon.sehier@camptocamp.com>
2017-06-05 10:25:08 +0200

    awesomerc update

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -261,7 +261,9 @@ globalkeys = awful.util.table.join(
         end),
 
     -- Standard program
-    awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+    awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal,{
+        tag       = mouse.screen.selected_tag,
+    }) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
